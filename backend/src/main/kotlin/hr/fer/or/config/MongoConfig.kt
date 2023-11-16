4518d20138e31edb631ecd506364de5def0c23e3
Marko Dodik <marko.dodik21@gmail.com>
2023-11-16 12:56:11 +0100

    Mongo configuration fix

--- backend/src/main/kotlin/hr/fer/or/config/MongoConfig.kt
+++ backend/src/main/kotlin/hr/fer/or/config/MongoConfig.kt
@@ -17,21 +17,21 @@ class MongoConfig {
     @Bean
     @Singleton
     fun mongoClient(
-        @Value("\${mongo.uri}") uri: String
-    ): CoroutineClient = KMongo.createClient(uri).coroutine
+        //@Value("\${mongo.uri}") uri: String
+    ): CoroutineClient = KMongo.createClient("mongodb://localhost:27017/?minPoolSize=12&maxPoolSize=12&authSource=admin").coroutine
 
     @Bean
     @Singleton
     fun mongoDatabase(
-        @Value("\${mongo.database}") database: String,
+        //@Value("\${mongo.database}") database: String,
         mongoClient: CoroutineClient
-    ) = mongoClient.getDatabase(database)
+    ) = mongoClient.getDatabase("teams")
 
     @Bean
-    @Named("premierLeagueCollection")
+    @Named("teamCollection")
     @Singleton
     fun teamCollection(
         mongoDatabase: CoroutineDatabase
-    ) = mongoDatabase.getCollection<Team>("premierLeagueCollection")
+    ) = mongoDatabase.getCollection<Team>("teamCollection")
 
 }
