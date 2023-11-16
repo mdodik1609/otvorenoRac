21919aa2d26fbd86f345ea069d1e3269499639b1
Marko Dodik <marko.dodik21@gmail.com>
2023-11-16 12:55:35 +0100

    teamRepo update to inluce get/save/del methods

--- backend/src/main/kotlin/hr/fer/or/repository/TeamRepository.kt
+++ backend/src/main/kotlin/hr/fer/or/repository/TeamRepository.kt
@@ -3,6 +3,8 @@ package hr.fer.or.repository
 import hr.fer.or.model.Team
 import org.litote.kmongo.coroutine.CoroutineCollection
 import jakarta.inject.Singleton
+import org.litote.kmongo.eq
+import org.litote.kmongo.id.IdGenerator
 
 @Singleton
 class TeamRepository(
@@ -10,4 +12,17 @@ class TeamRepository(
 ) {
 
     suspend fun findAll(): List<Team> = teamCollection.find().toList()
+
+    suspend fun findOneById(teamId: String) = teamCollection.findOne(Team::_id eq IdGenerator.defaultGenerator.create(teamId))
+
+    suspend fun findOneByName(teamName: String) = teamCollection.findOne(Team::teamName eq teamName)
+
+    suspend fun findOneByPlace(place: Int) = teamCollection.findOne(Team::place eq place)
+
+    suspend fun findByPoints(points: Int) = teamCollection.findOne(Team::points eq points)
+
+    suspend fun createOrUpdateTeam(team: Team) = teamCollection.save(team)
+
+    suspend fun deleteTeamById(teamId: String) = teamCollection.deleteOne(Team::_id eq IdGenerator.defaultGenerator.create(teamId))
+
 }
